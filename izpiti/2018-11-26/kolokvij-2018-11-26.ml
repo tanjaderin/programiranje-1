29528b92b49f3081144b8e442c08ad9bd95f1ef3
tanjaderin <tanja.derin88@gmail.com>
2019-11-17 23:09:26 +0100

    vaja

--- izpiti/2018-11-26/kolokvij-2018-11-26.ml
+++ izpiti/2018-11-26/kolokvij-2018-11-26.ml
@@ -1,12 +1,42 @@
 (* -------- 1 -------- *)
-
+let rec vsota sez = 
+  let rec vsota' acc sez =
+    match sez with
+    |[] -> acc
+    |x :: xs -> vsota' (x + acc) xs
+  in
+  vsota' 0 sez
+ 
 (* -------- 2 -------- *)
+let preveri sez =
+  match sez with
+  |[] -> true
+  | x :: y :: xs when x <= y -> preveri (y :: xs)
+  | _ -> false
 
 (* -------- 3 -------- *)
 
+
+let rec vstavi stevilo seznam = 
+  let rec vstavi' acc stevilo seznam =
+  match seznam with 
+  | [] -> acc
+  | x :: xs when x > stevilo -> vstavi' (stevilo :: x :: acc) stevilo xs
+  | x :: xs -> vstavi' (x :: acc) stevilo xs
+  in 
+  vstavi' [] stevilo seznam
+
 (* -------- 4 -------- *)
+*(let rec urejanje cmp sez = *)
 
 (* -------- 5 -------- *)
+type priority =
+  |Top
+  |Group of int
+
+type status =
+  |Staff
+  |Passenger of priority
 
 type flyer = { status : status ; name : string }
 
@@ -19,5 +49,6 @@ let flyers = [ {status = Staff; name = "Quinn"}
              ]
 
 (* -------- 6 -------- *)
-
+let uredi = function
+  |{status tip}
 (* -------- 7 -------- *)
